f4f49d724576282eb3b33ada160f6c360afdb0d6
lingtoh <lingtoh@gmail.com>
2017-08-24 12:26:13 -0700

    Added exception when FunctionType is unknown.

--- src/WebJobs.Extensions.DurableTask/DurableOrchestrationContext.cs
+++ src/WebJobs.Extensions.DurableTask/DurableOrchestrationContext.cs
@@ -247,8 +247,9 @@ namespace Microsoft.Azure.WebJobs
                     callTask = this.innerContext.CreateSubOrchestrationInstance<TResult>(functionName, version, JsonConvert.SerializeObject(parameters));
                     break;
                 default:
-                    callTask = null;
-                    break;
+                    throw new ArgumentException(
+                        string.Format("The function '{0}' doesn't exist, is disabled, or is not an activity or orchestrator function",
+                            functionName));
             }
 
             string sourceFunctionId = string.IsNullOrEmpty(this.orchestrationVersion) ?
