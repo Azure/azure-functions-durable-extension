9fce366618a61899d83ab32929c9490f337bae73
Chris Gillum <cgillum@gmail.com>
2017-09-29 14:27:39 -0700

    Updated precompiled sampled to work with .net core. Also updated nuget version.

--- samples/precompiled/PhoneVerification.cs
+++ samples/precompiled/PhoneVerification.cs
@@ -6,8 +6,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+#if NETSTANDARD2_0
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
+#else
+using Twilio;
+#endif
 
 namespace VSSample
 {
@@ -72,7 +76,12 @@ namespace VSSample
         public static int SendSmsChallenge(
             [ActivityTrigger] string phoneNumber,
             TraceWriter log,
-            [TwilioSms(AccountSidSetting = "TwilioAccountSid", AuthTokenSetting = "TwilioAuthToken", From = "%TwilioPhoneNumber%")] out CreateMessageOptions message)
+            [TwilioSms(AccountSidSetting = "TwilioAccountSid", AuthTokenSetting = "TwilioAuthToken", From = "%TwilioPhoneNumber%")]
+#if NETSTANDARD2_0
+                out CreateMessageOptions message)
+#else
+                out SMSMessage message)
+#endif
         {
             // Get a random number generator with a random seed (not time-based)
             var rand = new Random(Guid.NewGuid().GetHashCode());
@@ -80,7 +89,11 @@ namespace VSSample
 
             log.Info($"Sending verification code {challengeCode} to {phoneNumber}.");
 
+#if NETSTANDARD2_0
             message = new CreateMessageOptions(new PhoneNumber(phoneNumber));
+#else
+            message = new SMSMessage { To = phoneNumber };
+#endif
             message.Body = $"Your verification code is {challengeCode:0000}";
 
             return challengeCode;
