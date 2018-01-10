5c9e278419ba5b834e8a1a8841af5a3f0e95f2b1
gled4er <kadimitr@microsoft.com>
2018-01-10 16:56:16 +0900

    Adding copyright headers.

--- test/DurableOrchestrationClientBaseTests.cs
+++ test/DurableOrchestrationClientBaseTests.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using System.Net.Http;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Azure.WebJobs;
