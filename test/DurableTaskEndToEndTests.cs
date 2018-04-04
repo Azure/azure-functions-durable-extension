3edb791c5e4fb6d0bf86fe1e8ec4bdcc633cd4bf
Kanio Dimitrov <kadimitr@microsoft.com>
2018-04-04 17:04:52 +0900

    Small bump on the timeout settings for a failing test.

--- test/DurableTaskEndToEndTests.cs
+++ test/DurableTaskEndToEndTests.cs
@@ -485,7 +485,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask.Tests
                 {
                     var timeout = TimeSpan.FromSeconds(10);
                     var client = await host.StartOrchestratorAsync(nameof(TestOrchestrations.Approval), timeout, this.output);
-                    await client.WaitForCompletionAsync(TimeSpan.FromSeconds(100), this.output);
+                    await client.WaitForCompletionAsync(TimeSpan.FromSeconds(60), this.output);
 
                     // Don't send any notification - let the internal timeout expire
                 };
@@ -497,9 +497,9 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask.Tests
                     tasks[i] = orchestrationStarter();
                 }
 
-                // The 100 orchestrations above (which each delay for 10 seconds) should all complete in less than 70 seconds.
+                // The 100 orchestrations above (which each delay for 10 seconds) should all complete in less than 80 seconds.
                 Task parallelOrchestrations = Task.WhenAll(tasks);
-                Task timeoutTask = Task.Delay(TimeSpan.FromSeconds(70));
+                Task timeoutTask = Task.Delay(TimeSpan.FromSeconds(80));
 
                 Task winner = await Task.WhenAny(parallelOrchestrations, timeoutTask);
                 Assert.Equal(parallelOrchestrations, winner);
