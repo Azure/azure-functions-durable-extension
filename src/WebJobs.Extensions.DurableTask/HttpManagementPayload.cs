f57726043c453d342bf0a2cb816e74f28dfbb403
Chris Gillum <cgillum@gmail.com>
2018-06-21 17:35:09 -0700

    Update samples and auto-generated docs for v1.5.0

--- src/WebJobs.Extensions.DurableTask/HttpManagementPayload.cs
+++ src/WebJobs.Extensions.DurableTask/HttpManagementPayload.cs
@@ -13,24 +13,36 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
         /// <summary>
         /// Gets the ID of the orchestration instance.
         /// </summary>
+        /// <value>
+        /// The ID of the orchestration instance.
+        /// </value>
         [JsonProperty("id")]
         public string Id { get; internal set; }
 
         /// <summary>
         /// Gets the HTTP GET status query endpoint URL.
         /// </summary>
+        /// <value>
+        /// The HTTP URL for fetching the instance status.
+        /// </value>
         [JsonProperty("statusQueryGetUri")]
         public string StatusQueryGetUri { get; internal set; }
 
         /// <summary>
         /// Gets the HTTP POST external event sending endpoint URL.
         /// </summary>
+        /// <value>
+        /// The HTTP URL for posting external event notifications.
+        /// </value>
         [JsonProperty("sendEventPostUri")]
         public string SendEventPostUri { get; internal set; }
 
         /// <summary>
         /// Gets the HTTP POST instance termination endpoint.
         /// </summary>
+        /// <value>
+        /// The HTTP URL for posting instance termination commands.
+        /// </value>
         [JsonProperty("terminatePostUri")]
         public string TerminatePostUri { get; internal set; }
     }
