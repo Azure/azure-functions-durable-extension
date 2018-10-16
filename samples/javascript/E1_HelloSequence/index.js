664ba39eb1f36adeab57813abadb44fec030b627
Katy Shimizu <katy.shimizu@microsoft.com>
2018-10-16 12:58:13 -0700

    Updated samples for Functions v2 and durable-functions 1.0.0. Resolves Azure/azure-docs#16903

--- samples/javascript/E1_HelloSequence/index.js
+++ samples/javascript/E1_HelloSequence/index.js
@@ -1,11 +1,11 @@
 const df = require("durable-functions");
 
-module.exports = df(function*(context){
+module.exports = df.orchestrator(function*(context){
     context.log("Starting chain sample");
     const output = [];
-    output.push(yield context.df.callActivityAsync("E1_SayHello", "Tokyo"));
-    output.push(yield context.df.callActivityAsync("E1_SayHello", "Seattle"));
-    output.push(yield context.df.callActivityAsync("E1_SayHello", "London"));
+    output.push(yield context.df.callActivity("E1_SayHello", "Tokyo"));
+    output.push(yield context.df.callActivity("E1_SayHello", "Seattle"));
+    output.push(yield context.df.callActivity("E1_SayHello", "London"));
 
     return output;
 });
