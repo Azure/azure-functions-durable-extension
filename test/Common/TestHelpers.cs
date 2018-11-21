3b1927536df891f3c3e8dd617414abb920af8912
Kanio Dimitrov <kadimitr@microsoft.com>
2018-11-21 14:24:23 +0000

    Modifying comments.

--- test/Common/TestHelpers.cs
+++ test/Common/TestHelpers.cs
@@ -643,7 +643,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask.Tests
     }
 
     /// <summary>
-    /// Helper implementation of INameResolver interface.
+    /// Test helper implementation of INameResolver interface.
     /// </summary>
     public class SimpleNameResolver : INameResolver
     {
