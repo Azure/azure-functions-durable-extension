fc44e9eced7b23f5dfdccdd8b949a975b9d1b181
Sebastian Burckhardt <sburckha@microsoft.com>
2019-06-13 09:32:35 -0700

    Draft of how to use proxies in a client context for signaling.

--- src/WebJobs.Extensions.DurableTask/EntityScheduler/Proxy/OrchestrationClientProxy.cs
+++ src/WebJobs.Extensions.DurableTask/EntityScheduler/Proxy/OrchestrationClientProxy.cs
@@ -15,19 +15,33 @@ namespace Microsoft.Azure.WebJobs
             this.client = client;
         }
 
+        internal Task SignalTask { get; private set; }
+
         public Task CallAsync(EntityId entityId, string operationName, object operationInput)
         {
-            throw new NotSupportedException();
+            this.SignalAndStoreTask(entityId, operationName, operationInput);
+            return Task.CompletedTask;
         }
 
         public Task<TResult> CallAsync<TResult>(EntityId entityId, string operationName, object operationInput)
         {
-            throw new NotSupportedException();
+            this.SignalAndStoreTask(entityId, operationName, operationInput);
+            return Task.FromResult<TResult>(default(TResult));
         }
 
         public void Signal(EntityId entityId, string operationName, object operationInput)
         {
-            this.client.SignalEntityAsync(entityId, operationName, operationInput).GetAwaiter().GetResult();
+            this.SignalAndStoreTask(entityId, operationName, operationInput);
+        }
+
+        private void SignalAndStoreTask(EntityId entityId, string operationName, object operationInput)
+        {
+            if (this.SignalTask != null)
+            {
+                throw new InvalidOperationException("The operation action must not perform more than one operation");
+            }
+
+            this.SignalTask = this.client.SignalEntityAsync(entityId, operationName, operationInput);
         }
     }
 }
