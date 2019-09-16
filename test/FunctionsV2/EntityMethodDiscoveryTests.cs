272a7e3aab6e2f254e8776764c894a3e618143e5
Aaron Powell <me@aaron-powell.com>
2019-09-17 08:56:51 +1000

    Adding standard license header to the test file

--- test/FunctionsV2/EntityMethodDiscoveryTests.cs
+++ test/FunctionsV2/EntityMethodDiscoveryTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Azure.WebJobs;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask.Tests;
 using Moq;
 using Xunit;
