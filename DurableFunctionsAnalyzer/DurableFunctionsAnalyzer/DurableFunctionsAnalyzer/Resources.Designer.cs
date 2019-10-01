735474603011cb4c0e95f457e29fd43fc5a5ab90
amdeel <52223332+amdeel@users.noreply.github.com>
2019-10-01 09:42:01 -0700

    switched back to .net standard 1.3

--- DurableFunctionsAnalyzer/DurableFunctionsAnalyzer/DurableFunctionsAnalyzer/Resources.Designer.cs
+++ DurableFunctionsAnalyzer/DurableFunctionsAnalyzer/DurableFunctionsAnalyzer/Resources.Designer.cs
@@ -10,8 +10,9 @@
 
 namespace WebJobs.Extensions.DurableTask.Analyzers {
     using System;
-    
-    
+    using System.Reflection;
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -39,7 +40,7 @@ namespace WebJobs.Extensions.DurableTask.Analyzers {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WebJobs.Extensions.DurableTask.Analyzers.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WebJobs.Extensions.DurableTask.Analyzers.Resources", typeof(Resources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
