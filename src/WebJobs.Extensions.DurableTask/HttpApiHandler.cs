a4dd06bd228d1db875da886d91ecd16d0f48770c
Anthony Chu <antchu@microsoft.com>
2019-12-03 19:40:32 +0000

    Remove unused using

--- src/WebJobs.Extensions.DurableTask/HttpApiHandler.cs
+++ src/WebJobs.Extensions.DurableTask/HttpApiHandler.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
