aa4fa324cc75ccacb0cc7ffa44f8e3e9fc42d818
David Revoledo <davidrevoledo@d-genix.com>
2020-01-09 19:20:16 -0300

    Add Parameterless method to IDurableClientFactory

--- src/WebJobs.Extensions.DurableTask/ContextImplementations/IDurableClientFactory.cs
+++ src/WebJobs.Extensions.DurableTask/ContextImplementations/IDurableClientFactory.cs
@@ -16,5 +16,11 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask.ContextImplementations
         /// <param name="durableClientOptions">options containing the client configuration parameters.</param>
         /// <returns>Returns a <see cref="IDurableClient"/> instance. The returned instance may be a cached instance.</returns>
         IDurableClient CreateClient(DurableClientOptions durableClientOptions);
+
+        /// <summary>
+        /// Gets a <see cref="IDurableClient"/> using configuration from a <see cref="DurableClientOptions"/> instance.
+        /// </summary>
+        /// <returns>Returns a <see cref="IDurableClient"/> instance. The returned instance may be a cached instance.</returns>
+        IDurableClient CreateClient();
     }
 }
