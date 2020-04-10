e283b57042bb8b1bb71f3d880bb8eeef5d69ebb7
Amanda Deel <52223332+amdeel@users.noreply.github.com>
2020-04-10 11:19:02 -0700

    Added use of custom error serializer in DTFx. (#1294)

--- src/WebJobs.Extensions.DurableTask/Listener/TaskEntityShim.cs
+++ src/WebJobs.Extensions.DurableTask/Listener/TaskEntityShim.cs
@@ -156,7 +156,12 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
 
             if (!this.messageDataConverter.IsDefault)
             {
-                innerContext.DataConverter = this.messageDataConverter;
+                innerContext.MessageDataConverter = this.messageDataConverter;
+            }
+
+            if (!this.errorDataConverter.IsDefault)
+            {
+                innerContext.ErrorDataConverter = this.errorDataConverter;
             }
 
             this.Config.TraceHelper.FunctionStarting(
