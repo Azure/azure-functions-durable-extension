963f77d3894e071ae877c65eac09fa9cdb4376b5
David Justo <david.justo.1996@gmail.com>
2020-04-23 17:07:13 -0700

    removed unecessary imports

--- src/WebJobs.Extensions.DurableTask/Listener/TaskOrchestrationShim.cs
+++ src/WebJobs.Extensions.DurableTask/Listener/TaskOrchestrationShim.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
-using System.Diagnostics;
-using System.Linq;
 using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
 using DurableTask.Core;
