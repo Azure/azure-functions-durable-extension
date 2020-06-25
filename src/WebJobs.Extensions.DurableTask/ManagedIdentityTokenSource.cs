286cdb7319e22ad699ad2b7de08e856a6c4bff08
Varshi Bachu <vabachu@microsoft.com>
2020-06-25 16:10:04 -0700

    token is retrieved using ManagedIdentityCredential now

--- src/WebJobs.Extensions.DurableTask/ManagedIdentityTokenSource.cs
+++ src/WebJobs.Extensions.DurableTask/ManagedIdentityTokenSource.cs
@@ -3,7 +3,8 @@
 
 using System;
 using System.Threading.Tasks;
-using Microsoft.Azure.Services.AppAuthentication;
+using Azure.Core;
+using Azure.Identity;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -36,8 +37,13 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
         /// <inheritdoc/>
         public async Task<string> GetTokenAsync()
         {
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync(this.Resource);
+            var scopes = new string[] { this.Resource };
+            TokenRequestContext context = new TokenRequestContext(scopes);
+
+            ManagedIdentityCredential credential = new ManagedIdentityCredential();
+            AccessToken token = await credential.GetTokenAsync(context);
+            string accessToken = token.Token;
+
             return accessToken;
         }
     }
