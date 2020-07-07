9e5f3f464c0cb59bd93e5da4246b6ba2758ae71b
Varshi Bachu <vabachu@microsoft.com>
2020-07-07 16:27:22 -0700

    removed Azure.Identity using directive from ITokenSource

--- src/WebJobs.Extensions.DurableTask/ITokenSource.cs
+++ src/WebJobs.Extensions.DurableTask/ITokenSource.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Threading.Tasks;
-using Azure.Identity;
 
 namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
 {
