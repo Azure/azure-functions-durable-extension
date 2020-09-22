8ecbbe84218c9faca6bdaf3c97d83641de71f736
David Justo <david.justo.1996@gmail.com>
2020-09-22 15:08:20 -0700

    added ETW event listener

---
+++ src/WebJobs.Extensions.DurableTask/ETWEventListener.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
+{
+    internal class ETWEventListener : EventListener
+    {
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            ILogger logger = null; // TODO: need to set
+            bool inLinux = true; // TODO: need to set
+            if (inLinux)
+            {
+                // TODO: Not sure that LogLevel here is right
+                logger.Log(LogLevel.Information, eventData.EventId, eventData, null, null);
+            }
+        }
+    }
+}
