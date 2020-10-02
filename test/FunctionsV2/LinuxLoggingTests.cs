07caf7c6485918ed4270b6eb2c905c8ac22e68d2
David Justo <dajusto@microsoft.com>
2020-10-02 13:15:14 -0700

    skeleton on logging tests

---
+++ test/FunctionsV2/LinuxLoggingTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace WebJobs.Extensions.DurableTask.Tests
+{
+    public class LinuxLoggingTests
+    {
+
+        [Fact]
+        public void ArchivesMultipleFiles()
+        {
+            // TODO:
+        }
+
+        [Fact]
+        public void DeletesArchives()
+        {
+
+        }
+
+        [Fact]
+        public void CreatesNewLoggingFile()
+        {
+            // TODO:
+        }
+
+        [Fact]
+        public void WritesToConsole()
+        {
+            // TODO:
+        }
+
+        [Fact]
+        public void WritesToFile()
+        {
+            // TODO:
+        }
+
+        [Fact]
+        public void DetectsLinuxConsumption()
+        {
+            // TODO:
+        }
+
+        [Fact]
+        public void DetectsLinuxDedicated()
+        {
+            // TODO:
+        }
+
+        [Fact]
+        public void DetectsNotALinuxHost()
+        {
+            // TODO:
+        }
+    }
+}
