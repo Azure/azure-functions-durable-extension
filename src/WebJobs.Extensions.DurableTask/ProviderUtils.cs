5c0d9e626d3ded32a12ef30fb5abefe027019de7
Sebastian Burckhardt <sburckha@microsoft.com>
2021-02-19 10:29:05 -0800

    add a mechanism for providers to override the scale monitor.

--- src/WebJobs.Extensions.DurableTask/ProviderUtils.cs
+++ src/WebJobs.Extensions.DurableTask/ProviderUtils.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using DurableTask.Core;
+#if !FUNCTIONS_V1
+using Microsoft.Azure.WebJobs.Host.Scale;
+#endif
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -16,6 +19,22 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
     /// </summary>
     public static class ProviderUtils
     {
+#if !FUNCTIONS_V1
+        /// <summary>
+        /// Providers can implement this interface to override the autoscaling.
+        /// </summary>
+        public interface IProviderWithAutoScaling
+        {
+            /// <summary>
+            /// Tries to obtain a scale monitor for autoscaling.
+            /// </summary>
+            /// <param name="extension">The extension, which is used for tracing.</param>
+            /// <param name="scaleMonitor">The scale monitor.</param>
+            /// <returns>True if autoscaling is supported, false otherwise.</returns>
+            bool TryGetScaleMonitor(DurableTaskExtension extension, out IScaleMonitor scaleMonitor);
+        }
+#endif
+
         /// <summary>
         /// Returns the instance id of the entity scheduler for a given entity id.
         /// </summary>
@@ -60,6 +79,33 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
         public static DurableOrchestrationStatus ConvertOrchestrationStateToStatus(OrchestrationState orchestrationState)
         {
             return DurableClient.ConvertOrchestrationStateToStatus(orchestrationState);
+        }
+
+        /// <summary>
+        /// Trace an informational event for the extension.
+        /// </summary>
+        /// <param name="extension">The durable task extension.</param>
+        /// <param name="hubName">The name of the task hub.</param>
+        /// <param name="instanceId">The instance id.</param>
+        /// <param name="functionName">The function name.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="writeToUserLogs">Whether to write this event to the user logs.</param>
+        public static void TraceInformationalEvent(this DurableTaskExtension extension, string hubName, string instanceId, string functionName, string message, bool writeToUserLogs)
+        {
+            extension.TraceHelper.ExtensionInformationalEvent(hubName, instanceId, functionName, message, writeToUserLogs);
+        }
+
+        /// <summary>
+        /// Trace a warning event for the extension.
+        /// </summary>
+        /// <param name="extension">The durable task extension.</param>
+        /// <param name="hubName">The name of the task hub.</param>
+        /// <param name="functionName">The function name.</param>
+        /// <param name="instanceId">The instance id.</param>
+        /// <param name="message">The message.</param>
+        public static void TraceWarningEvent(this DurableTaskExtension extension, string hubName, string functionName, string instanceId, string message)
+        {
+            extension.TraceHelper.ExtensionWarningEvent(hubName, functionName, instanceId, message);
         }
     }
 }
