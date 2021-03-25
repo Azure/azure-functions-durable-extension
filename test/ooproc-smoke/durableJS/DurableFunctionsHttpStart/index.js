09e7c30bdd253371948e5c6347ff58aae4f5a246
David Justo <david.justo.1996@gmail.com>
2021-03-25 14:41:49 -0700

    Add initial draft of ooproc smoke tests

---
+++ test/ooproc-smoke/durableJS/DurableFunctionsHttpStart/index.js
@@ -0,0 +1,10 @@
+﻿const df = require("durable-functions");
+
+module.exports = async function (context, req) {
+    const client = df.getClient(context);
+    const instanceId = await client.startNew(req.params.functionName, undefined, req.body);
+
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+    return client.createCheckStatusResponse(context.bindingData.req, instanceId);
+};
