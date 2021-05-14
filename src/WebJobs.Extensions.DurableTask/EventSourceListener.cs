ecec79d2aa36f8a52d6bddf6ff6baa88e6aa1a74
David Justo <david.justo.1996@gmail.com>
2021-05-14 16:10:48 -0700

    Filter ETW

--- src/WebJobs.Extensions.DurableTask/EventSourceListener.cs
+++ src/WebJobs.Extensions.DurableTask/EventSourceListener.cs
@@ -39,8 +39,9 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
         /// <param name="eventSource">An instance of EventSource.</param>
         protected override void OnEventSourceCreated(EventSource eventSource)
         {
-
-            if (eventSource.Name == "DurableTask-Core" ||
+            if ((eventSource.Name == "DurableTask-Core"
+                && eventSource.Guid != new Guid("7DA4779A-152E-44A2-A6F2-F80D991A5BEE")) ||
+                eventSource.Name == "DurableTask-Core" ||
                 eventSource.Name == "DurableTask-AzureStorage" ||
                 eventSource.Name == "WebJobs-Extensions-DurableTask" ||
                 eventSource.Name == "DurableTask-SqlServer")
