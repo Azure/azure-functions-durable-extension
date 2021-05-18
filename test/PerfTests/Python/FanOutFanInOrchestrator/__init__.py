7346570bd4683d309a82000a1ed7a06cc7aa01ac
David Justo <david.justo.1996@gmail.com>
2021-05-18 11:30:50 -0700

    remove leftover comments

--- test/PerfTests/Python/FanOutFanInOrchestrator/__init__.py
+++ test/PerfTests/Python/FanOutFanInOrchestrator/__init__.py
@@ -1,11 +1,3 @@
-# This function is not intended to be invoked directly. Instead it will be
-# triggered by an HTTP starter function.
-# Before running this sample, please:
-# - create a Durable activity function (default name is "Hello")
-# - create a Durable HTTP starter function
-# - add azure-functions-durable to requirements.txt
-# - run pip install -r requirements.txt
-
 import logging
 import json
 
