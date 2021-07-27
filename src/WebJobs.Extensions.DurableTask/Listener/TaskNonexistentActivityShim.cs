446b25f2783e42d10871d5918f5ed8bc6c1ea0bd
Varshi Bachu <vabachu@microsoft.com>
2021-07-27 11:31:33 -0700

    added file header

--- src/WebJobs.Extensions.DurableTask/Listener/TaskNonexistentActivityShim.cs
+++ src/WebJobs.Extensions.DurableTask/Listener/TaskNonexistentActivityShim.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
 using System.Threading.Tasks;
 using DurableTask.Core;
 using DurableTask.Core.Common;
