bbe933f3d31c11e2b46d8b23c63ac56b99a9e9c0
Chris Gillum <cgillum@microsoft.com>
2022-01-21 10:39:40 -0800

    Add e2e-test error handling for better diagnostics

--- test/SmokeTests/OOProcSmokeTests/DotNetIsolated/e2e-test.ps1
+++ test/SmokeTests/OOProcSmokeTests/DotNetIsolated/e2e-test.ps1
@@ -1,4 +1,6 @@
-﻿param(
+﻿# Installing PowerShell: https://docs.microsoft.com/powershell/scripting/install/installing-powershell
+
+param(
     [string]$imageName="dfapp-dotnet-isolated"
 )
 
@@ -13,10 +15,11 @@ Write-Host "Pulling down the mcr.microsoft.com/azure-storage/azurite image..." -
 docker pull mcr.microsoft.com/azure-storage/azurite
 
 Write-Host "Starting Azurite storage emulator using default ports..." -ForegroundColor Yellow
-docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 -d mcr.microsoft.com/azure-storage/azurite
+docker run --name 'azurite' -p 10000:10000 -p 10001:10001 -p 10002:10002 -d mcr.microsoft.com/azure-storage/azurite
 
 # Finally, start up the smoke test container, which will connect to the Azurite container
-docker run -p 8080:80 -it --add-host=host.docker.internal:host-gateway -d `
+docker run --name 'app' -p 8080:80 -it --add-host=host.docker.internal:host-gateway -d `
+	--memory="512m" --cpus=1 `
 	--env 'AzureWebJobsStorage=UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://host.docker.internal' `
 	--env 'WEBSITE_HOSTNAME=localhost:8080' `
 	$imageName
@@ -28,29 +31,42 @@ Start-Sleep -Seconds 60
 # Check to see what containers are running
 docker ps
 
-$startOrchestrationUri = "http://localhost:8080/api/StartHelloCitiesTyped"
-Write-Host "Starting a new orchestration instance via POST to $startOrchestrationUri..." -ForegroundColor Yellow
+try {
+	# Note that any HTTP protocol errors (e.g. HTTP 4xx or 5xx) will cause an immediate failure
+	$startOrchestrationUri = "http://localhost:8080/api/StartHelloCitiesTyped"
+	Write-Host "Starting a new orchestration instance via POST to $startOrchestrationUri..." -ForegroundColor Yellow
 
-$result = Invoke-RestMethod -Method Post -Uri $startOrchestrationUri
-Write-Host "Started orchestration with instance ID '$($result.id)'!" -ForegroundColor Yellow
-Write-Host "Waiting for orchestration to complete..." -ForegroundColor Yellow
+	$result = Invoke-RestMethod -Method Post -Uri $startOrchestrationUri
+	Write-Host "Started orchestration with instance ID '$($result.id)'!" -ForegroundColor Yellow
+	Write-Host "Waiting for orchestration to complete..." -ForegroundColor Yellow
 
-$retryCount = 0
-$success = $false
-$statusUrl = $result.statusQueryGetUri
+	$retryCount = 0
+	$success = $false
+	$statusUrl = $result.statusQueryGetUri
 
-while ($retryCount -lt 15) {
-	$result = Invoke-RestMethod -Method Get -Uri $statusUrl
-	$runtimeStatus = $result.runtimeStatus
-	Write-Host "Orchestration is $runtimeStatus" -ForegroundColor Yellow
+	while ($retryCount -lt 15) {
+		$result = Invoke-RestMethod -Method Get -Uri $statusUrl
+		$runtimeStatus = $result.runtimeStatus
+		Write-Host "Orchestration is $runtimeStatus" -ForegroundColor Yellow
 
-	if ($result.runtimeStatus -eq "Completed") {
-		$success = $true
-		break
+		if ($result.runtimeStatus -eq "Completed") {
+			$success = $true
+			break
+		}
+
+		Start-Sleep -Seconds 1
+		$retryCount = $retryCount + 1
 	}
+} catch {
+	Write-Host "An error occurred:" -ForegroundColor Red
+	Write-Host $_ -ForegroundColor Red
 
-	Start-Sleep -Seconds 1
-	$retryCount = $retryCount + 1
+	# Dump the docker logs to make debugging the issue easier
+	Write-Host "Below are the docker logs for the app container:" -ForegroundColor Red
+	docker logs 'app'
+
+	# Rethrow the original exception
+	throw
 }
 
 if ($success -eq $false) {
