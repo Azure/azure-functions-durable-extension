5ad65128096184eb35a89e2a66e76cf51d6d30cd
Chris Gillum <cgillum@microsoft.com>
2022-01-21 14:55:22 -0800

    Remove DotNetIsolated project from Release build Add parameters to e2e-test.ps1

--- test/SmokeTests/OOProcSmokeTests/DotNetIsolated/e2e-test.ps1
+++ test/SmokeTests/OOProcSmokeTests/DotNetIsolated/e2e-test.ps1
@@ -1,31 +1,38 @@
 ﻿# Installing PowerShell: https://docs.microsoft.com/powershell/scripting/install/installing-powershell
 
 param(
-    [string]$imageName="dfapp-dotnet-isolated"
+    [string]$ImageName="dfapp-dotnet-isolated",
+	[string]$ContainerName="app",
+	[switch]$NoSetup=$false,
+	[int]$Sleep=30
 )
 
 $ErrorActionPreference = "Stop"
 
-# Build the docker image first, since that's the most critical step
-Write-Host "Building sample app Docker container..." -ForegroundColor Yellow
-docker build -f $PSScriptRoot/Dockerfile -t $imageName $PSScriptRoot/../../../../
+if ($NoSetup -eq $false) {
+	# Build the docker image first, since that's the most critical step
+	Write-Host "Building sample app Docker container..." -ForegroundColor Yellow
+	docker build -f $PSScriptRoot/Dockerfile -t $ImageName $PSScriptRoot/../../../../
 
-# Next, download and start the Azurite emulator Docker image
-Write-Host "Pulling down the mcr.microsoft.com/azure-storage/azurite image..." -ForegroundColor Yellow
-docker pull mcr.microsoft.com/azure-storage/azurite
+	# Next, download and start the Azurite emulator Docker image
+	Write-Host "Pulling down the mcr.microsoft.com/azure-storage/azurite image..." -ForegroundColor Yellow
+	docker pull mcr.microsoft.com/azure-storage/azurite
 
-Write-Host "Starting Azurite storage emulator using default ports..." -ForegroundColor Yellow
-docker run --name 'azurite' -p 10000:10000 -p 10001:10001 -p 10002:10002 -d mcr.microsoft.com/azure-storage/azurite
+	Write-Host "Starting Azurite storage emulator using default ports..." -ForegroundColor Yellow
+	docker run --name 'azurite' -p 10000:10000 -p 10001:10001 -p 10002:10002 -d mcr.microsoft.com/azure-storage/azurite
 
-# Finally, start up the smoke test container, which will connect to the Azurite container
-docker run --name 'app' -p 8080:80 -it --add-host=host.docker.internal:host-gateway -d `
-	--env 'AzureWebJobsStorage=UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://host.docker.internal' `
-	--env 'WEBSITE_HOSTNAME=localhost:8080' `
-	$imageName
+	# Finally, start up the smoke test container, which will connect to the Azurite container
+	docker run --name $ContainerName -p 8080:80 -it --add-host=host.docker.internal:host-gateway -d `
+		--env 'AzureWebJobsStorage=UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://host.docker.internal' `
+		--env 'WEBSITE_HOSTNAME=localhost:8080' `
+		$ImageName
+}
 
-# The container needs a bit more time before it can start receiving requests
-Write-Host "Sleeping for 30 seconds to let the container finish initializing..." -ForegroundColor Yellow
-Start-Sleep -Seconds 30
+if ($sleep -gt  0) {
+	# The container needs a bit more time before it can start receiving requests
+	Write-Host "Sleeping for $Sleep seconds to let the container finish initializing..." -ForegroundColor Yellow
+	Start-Sleep -Seconds $Sleep
+}
 
 # Check to see what containers are running
 docker ps
@@ -62,7 +69,7 @@ try {
 
 	# Dump the docker logs to make debugging the issue easier
 	Write-Host "Below are the docker logs for the app container:" -ForegroundColor Red
-	docker logs 'app'
+	docker logs $ContainerName
 
 	# Rethrow the original exception
 	throw
