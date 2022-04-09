3c68079b269f9b0785abc3bfef73b91e43686e95
hossam-nasr <hossam.nasr99@gmail.com>
2022-04-08 17:10:10 -0700

    update upper schema to V3

--- src/WebJobs.Extensions.DurableTask/Bindings/OrchestrationTriggerAttributeBindingProvider.cs
+++ src/WebJobs.Extensions.DurableTask/Bindings/OrchestrationTriggerAttributeBindingProvider.cs
@@ -195,7 +195,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
                     new JProperty("instanceId", arg.InstanceId),
                     new JProperty("isReplaying", arg.IsReplaying),
                     new JProperty("parentInstanceId", arg.ParentInstanceId),
-                    new JProperty("upperSchemaVersion", SchemaVersion.V2),
+                    new JProperty("upperSchemaVersion", SchemaVersion.V3),
                     new JProperty("longRunningTimerIntervalDuration", arg.LongRunningTimerIntervalDuration),
                     new JProperty("maximumShortTimerDuration", arg.MaximumShortTimerDuration));
                 return contextObject.ToString();
