3938c6a8d7c431899c0fd39fef1451ef6476143a
Chris Gillum <cgillum@microsoft.com>
2022-05-15 13:48:14 -0700

    In progress work - not yet functional

---
+++ src/Worker.Extensions.DurableTask/DurableTaskExtensionStartup.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Functions.Worker.Core;
+using Microsoft.Azure.Functions.Worker.Extensions.DurableTask;
+using Microsoft.Extensions.Hosting;
+
+[assembly: WorkerExtensionStartup(typeof(DurableTaskExtensionStartup))]
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.DurableTask;
+
+/// <summary>
+/// Startup class that registers the Durable Task middleware with the function app.
+/// </summary>
+public sealed class DurableTaskExtensionStartup : WorkerExtensionStartup
+{
+    /// <inheritdoc/>
+    public override void Configure(IFunctionsWorkerApplicationBuilder applicationBuilder)
+    {
+        applicationBuilder.UseMiddleware<DurableTaskFunctionsMiddleware>();
+    }
+}
