3c4bc19fc63f01c4f3fb695c1eea74076b5ffc02
naiyuanTIAN <naiyuantian@microsoft.com>
2022-11-17 16:54:26 -0800

    addhead

--- src/WebJobs.Extensions.DurableTask/InstanceFilterOption.cs
+++ src/WebJobs.Extensions.DurableTask/InstanceFilterOption.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
 {
     /// <summary>
-    /// Filter option setting for IDurableOrchestrationClient.GetStatusAsync.
+    /// Filter option setting for IDurableOrchestrationClient.QueryStatusAsync.
     /// </summary>
     public class InstanceFilterOption
     {
