d6b8c76abfeeb02fb60c4dca9eb6f8ffcfe79331
Jacob Viau <javia@microsoft.com>
2023-01-27 15:57:19 -0800

    Allow for orchestration input to be a parameter (#2337)

--- src/Worker.Extensions.DurableTask/DurableTaskFunctionsMiddleware.cs
+++ src/Worker.Extensions.DurableTask/DurableTaskFunctionsMiddleware.cs
@@ -9,8 +9,12 @@ using Microsoft.DurableTask.Worker.Grpc;
 
 namespace Microsoft.Azure.Functions.Worker.Extensions.DurableTask;
 
+/// <summary>
+/// A middleware to handle orchestration triggers.
+/// </summary>
 internal class DurableTaskFunctionsMiddleware : IFunctionsWorkerMiddleware
 {
+    /// <inheritdoc />
     public async Task Invoke(FunctionContext functionContext, FunctionExecutionDelegate next)
     {
         if (!IsOrchestrationTrigger(functionContext, out BindingMetadata? triggerMetadata))
@@ -25,32 +29,16 @@ internal class DurableTaskFunctionsMiddleware : IFunctionsWorkerMiddleware
             throw new InvalidOperationException("Orchestration history state was either missing from the input or not a string value.");
         }
 
-        // Wrap the function implementation in a lambda orchestrator
-        string orchestratorOutput = GrpcOrchestrationRunner.LoadAndRun<object, object>(
-            encodedOrchestratorState,
-            async (orchestrationContext, input) =>
-            {
-                // Set the function input to be the orchestration context wrapped in our own object so that we can
-                // intercept any of the calls and inject our own logic or tracking.
-                FunctionsOrchestrationContext wrapperContext = new(orchestrationContext, functionContext);
-                triggerInputData.Value = wrapperContext;
-
-                // This method will advance to the next middleware and throw if it detects an asynchronous execution.
-                await EnsureSynchronousExecution(functionContext, next, wrapperContext);
-
-                // Set the raw function output as the orchestrator output
-                object? functionOutput = functionContext.GetInvocationResult().Value;
-                return functionOutput;
-            },
-            functionContext.InstanceServices);
+        FunctionsOrchestrator orchestrator = new(functionContext, next, triggerInputData);
+        string orchestratorOutput = GrpcOrchestrationRunner.LoadAndRun(
+            encodedOrchestratorState, orchestrator, functionContext.InstanceServices);
 
         // Send the encoded orchestrator output as the return value seen by the functions host extension
         functionContext.GetInvocationResult().Value = orchestratorOutput;
     }
 
     private static bool IsOrchestrationTrigger(
-        FunctionContext context,
-        [NotNullWhen(true)] out BindingMetadata? orchestrationTriggerBinding)
+        FunctionContext context, [NotNullWhen(true)] out BindingMetadata? orchestrationTriggerBinding)
     {
         foreach (BindingMetadata binding in context.FunctionDefinition.InputBindings.Values)
         {
@@ -63,26 +51,5 @@ internal class DurableTaskFunctionsMiddleware : IFunctionsWorkerMiddleware
 
         orchestrationTriggerBinding = null;
         return false;
-    }
-
-    private static async Task EnsureSynchronousExecution(
-        FunctionContext functionContext,
-        FunctionExecutionDelegate next,
-        FunctionsOrchestrationContext orchestrationContext)
-    {
-        Task orchestratorTask = next(functionContext);
-        if (!orchestratorTask.IsCompleted && !orchestrationContext.IsAccessed)
-        {
-            // If the middleware returns before the orchestrator function's context object was accessed and before
-            // it completes its execution, then we know that either some middleware component went async or that the
-            // orchestrator function did some illegal await as its very first action.
-            throw new InvalidOperationException(Constants.IllegalAwaitErrorMessage);
-        }
-
-        await orchestratorTask;
-
-        // This will throw if either the orchestrator performed an illegal await or if some middleware ahead of this
-        // one performed some illegal await.
-        orchestrationContext.ThrowIfIllegalAccess();
     }
 }
