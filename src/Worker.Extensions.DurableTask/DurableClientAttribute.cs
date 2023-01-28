2b136ebd0ebbe8ae1b27b9bd388e91a379ff561f
Jacob Viau <javia@microsoft.com>
2023-01-27 16:02:26 -0800

    Remove unused using

--- src/Worker.Extensions.DurableTask/DurableClientAttribute.cs
+++ src/Worker.Extensions.DurableTask/DurableClientAttribute.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure.Functions.Worker.Converters;
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
-using Microsoft.Azure.Functions.Worker.Extensions.DurableTask;
 
 namespace Microsoft.Azure.Functions.Worker;
 
