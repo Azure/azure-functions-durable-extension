4d4911f9f3a12f09178f09bce2e8ed03bb861e0e
Naiyuan Tian <110135109+nytiannn@users.noreply.github.com>
2023-07-10 15:22:43 -0700

    Update AzureStorageOptions.cs

--- src/WebJobs.Extensions.DurableTask/Options/AzureStorageOptions.cs
+++ src/WebJobs.Extensions.DurableTask/Options/AzureStorageOptions.cs
@@ -174,10 +174,10 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
 
         /// <summary>
         /// Determines whether or not to use the table partition management strategy.
-        /// The new partition manager is based on Azure Storage V2. And it's recommneded to use the new partition manager for Azure Storage V2.
+        /// The table-storage partition manager that help reduces costs for Azure Storage V2 account.
         /// </summary>
         /// /// <value>A boolean indicating whether we use the table partition strategy. Defaults to true.</value>
-        public bool UseTablePartitionManagement { get; set; } = true;
+        public bool UseTablePartitionManagement { get; set; } = false;
 
         /// <summary>
         /// Throws an exception if the provided hub name violates any naming conventions for the storage provider.
