d808c5612545dd3f6108afb206a7d4adee1c6d99
Will Sugarman <will.sugarman@microsoft.com>
2023-09-09 11:20:57 -0700

    Minor comments

--- src/WebJobs.Extensions.DurableTask/Storage/NameValidator.cs
+++ src/WebJobs.Extensions.DurableTask/Storage/NameValidator.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-
+#nullable enable
 using System;
 using System.Globalization;
 using System.Text.RegularExpressions;
