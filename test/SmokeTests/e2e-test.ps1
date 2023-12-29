a7485736e7e609c137ecd60140ef52a9cd5309b0
Varshitha Bachu <vabachu@microsoft.com>
2023-12-28 18:04:31 -0800

    Update e2e-test.ps1

--- test/SmokeTests/e2e-test.ps1
+++ test/SmokeTests/e2e-test.ps1
@@ -79,7 +79,7 @@ if ($MSSQLTest -eq $true) {
 	# Finally, start up the application container, connecting to the SQL Server container
 	Write-Host "Starting the $ContainerName application container" -ForegroundColor Yellow
  	docker run --name $ContainerName -p 8080:80 -it --add-host=host.docker.internal:host-gateway -d `
-	--env 'SQLDB_Connection=Server=mssql-server,1433;Database=$dbname;User=sa;Password=$pw;' `
+	--env 'SQLDB_Connection=Server=172.17.0.3,1433;Database=$dbname;User=sa;Password=$pw;' `
 	--env 'AzureWebJobsStorage=UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://host.docker.internal' `
 	--env 'WEBSITE_HOSTNAME=localhost:8080' `
 	$ImageName
