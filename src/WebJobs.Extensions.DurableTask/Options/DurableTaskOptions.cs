7369062b5b27cfb40edc520044502aa10149ff0b
Konstantin Gukov <gukkos@gmail.com>
2024-01-26 16:16:29 +0100

    Fail fast if `extendedSessions` is requested for a non-.NET worker.
    While the code does emit the warning and overwrites the option value, this is too late. The frameworks reads the "old" option value before the options are validated and actually runs as if the extended sessions are on. This leads to the hard-to-troubleshoot problems, e.g. the python worker never re-triggers the orchestration and never completes them.

--- src/WebJobs.Extensions.DurableTask/Options/DurableTaskOptions.cs
+++ src/WebJobs.Extensions.DurableTask/Options/DurableTaskOptions.cs
@@ -320,12 +320,10 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
                 runtimeLanguage != null && // If we don't know from the environment variable, don't assume customer isn't .NET
                 !string.Equals(runtimeLanguage, "dotnet", StringComparison.OrdinalIgnoreCase))
             {
-                traceHelper.ExtensionWarningEvent(
-                    hubName: this.HubName,
-                    functionName: string.Empty,
-                    instanceId: string.Empty,
-                    message: "Durable Functions does not work with extendedSessions = true for non-.NET languages. This value is being set to false instead. See https://docs.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-perf-and-scale#extended-sessions for more details.");
-                this.ExtendedSessionsEnabled = false;
+                throw new InvalidOperationException(
+                    "Durable Functions does not work with ExtendedSessionsEnabled = true for non-.NET languages. " +
+                    "This value is being set to false instead. " +
+                    "See https://docs.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-perf-and-scale#extended-sessions for more details.");
             }
 
             this.Notifications.Validate();
