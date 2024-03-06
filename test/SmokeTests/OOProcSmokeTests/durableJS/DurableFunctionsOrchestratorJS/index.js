0c9bafcdd8d5f3fe7f944fe65f76e212e39cde48
Varshitha Bachu <vabachu@microsoft.com>
2024-03-06 12:44:08 -0800

    Update index.js

--- test/SmokeTests/OOProcSmokeTests/durableJS/DurableFunctionsOrchestratorJS/index.js
+++ test/SmokeTests/OOProcSmokeTests/durableJS/DurableFunctionsOrchestratorJS/index.js
@@ -13,12 +13,15 @@ const df = require("durable-functions");
 
 module.exports = df.orchestrator(function* (context) {
     const outputs = [];
+    const city = {city:"Paris", country:"France"};
 
     // Replace "Hello" with the name of your Durable Activity Function.
     outputs.push(yield context.df.callActivity("Hello", "Tokyo"));
     outputs.push(yield context.df.callActivity("Hello", "Seattle"));
     outputs.push(yield context.df.callActivity("Hello", "London"));
+    outputs.push(yield context.df.callActivity("Hello", "123"));
+    outputs.push(yield context.df.callActivity("Hello", ["Dubai", "New York", "Vancouver"]));
+    outputs.push(yield context.df.callActivity("PrintObject", city));
 
-    // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
     return outputs;
 });
