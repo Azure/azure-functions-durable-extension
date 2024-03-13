254b2626c6696e709a65cc281e16fda99f535066
David Justo <david.justo.1996@gmail.com>
2024-03-13 16:20:38 -0700

    Patch deserialization bug in 2.13.1 (#2745)

--- test/SmokeTests/OOProcSmokeTests/durablePy/DurableFunctionsOrchestrator/__init__.py
+++ test/SmokeTests/OOProcSmokeTests/durablePy/DurableFunctionsOrchestrator/__init__.py
@@ -12,11 +12,34 @@ import json
 import azure.functions as func
 import azure.durable_functions as df
 
+class City:
+  def __init__(self, country, name):
+    self.country = country
+    self.name = name
+  
+  def to_json(self):
+        return json.dumps({"name": self.name, "country": self.country})
+    
+  @classmethod
+  def from_json(cls, json_str):
+      data = json.loads(json_str)
+      return cls(name=data['name'], country=data['country'])
+  
+  def __str__(self):
+        return f"City(name= {self.name}, country= {self.country})"
 
 def orchestrator_function(context: df.DurableOrchestrationContext):
     result1 = yield context.call_activity('Hello', "Tokyo")
     result2 = yield context.call_activity('Hello', "Seattle")
     result3 = yield context.call_activity('Hello', "London")
-    return [result1, result2, result3]
+    result4 = yield context.call_activity('Print', 123)
+
+    cities = ["Tokyo", "Seattle", "Cairo"]
+    result5 = yield context.call_activity("PrintArray", cities)
+
+    city = City("France", "Paris")
+    result6 = yield context.call_activity("PrintObject", city)
+
+    return [result1, result2, result3, result4, result5, result6]
 
 main = df.Orchestrator.create(orchestrator_function)
