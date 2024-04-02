42ff67807711a343d91499ec3b03d4cbc150bf24
Allan Targino <13934447+allantargino@users.noreply.github.com>
2024-04-02 10:38:45 -0300

    adding copyrights

--- test/Worker.Extensions.DurableTask.Analyzers.Test/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
+++ test/Worker.Extensions.DurableTask.Analyzers.Test/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Testing;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Testing.Verifiers;
 
--- test/Worker.Extensions.DurableTask.Analyzers.Test/Verifiers/CSharpCodeFixVerifier`2.cs
+++ test/Worker.Extensions.DurableTask.Analyzers.Test/Verifiers/CSharpCodeFixVerifier`2.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.CodeAnalysis;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
