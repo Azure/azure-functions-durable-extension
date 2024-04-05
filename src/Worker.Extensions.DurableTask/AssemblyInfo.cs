d70f4327481d9b605664c24a4f26476eba983790
David Justo <david.justo.1996@gmail.com>
2024-04-05 15:00:48 -0700

    automatically generate AssemblyInfo based on webjobs extensions csproj

--- src/Worker.Extensions.DurableTask/AssemblyInfo.cs
+++ src/Worker.Extensions.DurableTask/AssemblyInfo.cs
@@ -2,6 +2,4 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
-
-// TODO: Find a way to generate this dynamically at build-time
 [assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.DurableTask", "2.13.2")]
