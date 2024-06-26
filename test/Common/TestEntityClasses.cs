01b3c4958d7bb272195330c110fbdfcfffbfb186
naiyuantian@microsoft.com <naiyuantian@microsoft.com>
2024-06-25 21:59:09 -0700

    further update

--- test/Common/TestEntityClasses.cs
+++ test/Common/TestEntityClasses.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Specialized;
 using Newtonsoft.Json;
