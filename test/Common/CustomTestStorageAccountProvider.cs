32bb7ab68e124af71d8cd16ce2d9037452f45b3d
Tan Han Chong <hanchong.tan@nintex.com>
2024-10-01 15:28:59 +0800

    check connection string in StorageAccountDetails since its no longer assigned any value in StorageConnectionString

---
+++ test/Common/CustomTestStorageAccountProvider.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using DurableTask.AzureStorage;
+using Microsoft.WindowsAzure.Storage;
+
+namespace Microsoft.Azure.WebJobs.Extensions.DurableTask.Tests
+{
+    internal class CustomTestStorageAccountProvider : IStorageAccountProvider
+    {
+        private const string CustomConnectionString = "DefaultEndpointsProtocol=https;AccountName=test;AccountKey=dGVzdA==;EndpointSuffix=core.windows.net";
+        private readonly string customConnectionName;
+
+        public CustomTestStorageAccountProvider(string connectionName)
+        {
+            this.customConnectionName = connectionName;
+        }
+
+        public CloudStorageAccount GetCloudStorageAccount(string name) =>
+            CloudStorageAccount.Parse(name != this.customConnectionName ? TestHelpers.GetStorageConnectionString() : CustomConnectionString);
+
+        public StorageAccountDetails GetStorageAccountDetails(string name) =>
+            new StorageAccountDetails { ConnectionString = name != this.customConnectionName ? TestHelpers.GetStorageConnectionString() : CustomConnectionString };
+    }
+}
