412ff589612988aeddec29c2220d32b1232d50ea
Naiyuan Tian <110135109+nytian@users.noreply.github.com>
2024-10-21 16:20:16 -0700

    Add comment as suggested

--- src/Worker.Extensions.DurableTask/HttpManagementPayload.cs
+++ src/Worker.Extensions.DurableTask/HttpManagementPayload.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
+// This is a copy of: https://github.com/Azure/azure-functions-durable-extension/blob/dev/src/WebJobs.Extensions.DurableTask/HttpManagementPayload.cs
 
 using System;
 using System.Collections.Generic;
