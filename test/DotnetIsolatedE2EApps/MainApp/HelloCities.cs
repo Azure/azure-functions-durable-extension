9a6830385f3544bf0c7db016f2e2e407b0fd8bc4
Andy Staples <andystaples@microsoft.com>
2024-12-18 12:49:49 -0700

    Add license headers, remove unused files

--- test/DotnetIsolatedE2EApps/MainApp/HelloCities.cs
+++ test/DotnetIsolatedE2EApps/MainApp/HelloCities.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.DurableTask;
