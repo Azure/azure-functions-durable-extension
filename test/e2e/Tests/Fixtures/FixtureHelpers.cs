57a59d8c0065becaf083a94738daac6802744127
naiyuantian@microsoft.com <naiyuantian@microsoft.com>
2025-03-10 12:12:33 -0700

    add logs to check use dts backend

--- test/e2e/Tests/Fixtures/FixtureHelpers.cs
+++ test/e2e/Tests/Fixtures/FixtureHelpers.cs
@@ -99,7 +99,9 @@ public static class FixtureHelpers
                 funcProcess.StartInfo.EnvironmentVariables["AzureFunctionsJobHost__extensions__durableTask__storageProvider__connectionStringName"] = "SQLDB_Connection";
                 funcProcess.StartInfo.EnvironmentVariables["AzureFunctionsJobHost__extensions__durableTask__storageProvider__createDatabaseIfNotExists"] = "true";
                 return;
-            case "dts":
+            case "azuremanaged":
+                funcProcess.StartInfo.EnvironmentVariables["AzureFunctionsJobHost__logging__logLevel__default"] = "Debug";
+                funcProcess.StartInfo.EnvironmentVariables["AzureFunctionsJobHost__logging__logLevel__Host.Startup"] = "Debug";
                 funcProcess.StartInfo.EnvironmentVariables["AzureFunctionsJobHost__extensions__durableTask__hubName"] = "default";
                 funcProcess.StartInfo.EnvironmentVariables["AzureFunctionsJobHost__extensions__durableTask__storageProvider__type"] = "azureManaged";
                 funcProcess.StartInfo.EnvironmentVariables["AzureFunctionsJobHost__extensions__durableTask__storageProvider__connectionStringName"] = "DURABLE_TASK_SCHEDULER_CONNECTION_STRING";
