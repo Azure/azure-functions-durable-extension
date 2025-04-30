98326c862b4c2df5ef822804edfbd6ccc43ebe81
Andy Staples <andystaples@microsoft.com>
2025-04-30 10:14:23 -0600

    Add check for ACA for Linux logging

--- src/WebJobs.Extensions.DurableTask/DefaultPlatformInformation.cs
+++ src/WebJobs.Extensions.DurableTask/DefaultPlatformInformation.cs
@@ -64,9 +64,15 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask
             return inLinuxDedicated;
         }
 
+        private bool IsInAzureContainerApps()
+        {
+            string? managedEnvironmentValue = this.ReadEnviromentVariable("MANAGED_ENVIRONMENT");
+            return !string.IsNullOrEmpty(managedEnvironmentValue);
+        }
+
         public OperatingSystem GetOperatingSystem()
         {
-            if (this.IsInLinuxConsumption() || this.IsInLinuxAppService())
+            if (this.IsInLinuxConsumption() || this.IsInLinuxAppService() || this.IsInAzureContainerApps())
             {
                 return OperatingSystem.Linux;
             }
