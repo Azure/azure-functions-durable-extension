bfe8de866951d775e6736de7ce28286f61438edf
Sophia Tevosyan <stevosyan@microsoft.com>
2025-05-13 12:08:40 -0700

    making the start time nullable for the call we have in localgrpclistener in the case that a request time wasn't attached to the signal entity request

--- src/WebJobs.Extensions.DurableTask/Correlation/TraceHelper.cs
+++ src/WebJobs.Extensions.DurableTask/Correlation/TraceHelper.cs
@@ -48,7 +48,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask.Correlation
             return newActivity;
         }
 
-        internal static Activity? StartActivityForCallingOrSignalingEntity(string targetEntityId, string entityName, string operationName, bool signalEntity, DateTime? scheduledTime, ActivityContext? parentTraceContext, DateTimeOffset startTime = default, string? entityId = null)
+        internal static Activity? StartActivityForCallingOrSignalingEntity(string targetEntityId, string entityName, string operationName, bool signalEntity, DateTime? scheduledTime, ActivityContext? parentTraceContext, DateTimeOffset? startTime = default, string? entityId = null)
         {
             // We only want to create a trace activity for calling or signaling an entity in the case that we can successfully get the parent trace context of the request.
             // Otherwise, we will create an unlinked trace activity with no parent.
@@ -61,7 +61,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask.Correlation
                 Schema.SpanNames.CallOrSignalEntity(entityName, operationName),
                 kind: signalEntity ? ActivityKind.Producer : ActivityKind.Client,
                 parentContext: parentTraceContext.Value,
-                startTime: startTime);
+                startTime: startTime ?? default);
 
             if (newActivity == null)
             {
