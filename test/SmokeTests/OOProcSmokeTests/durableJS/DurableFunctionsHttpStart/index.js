ab2d02efa1e1e67a8cf85c30478c1c23062621df
naiyuantian@microsoft.com <naiyuantian@microsoft.com>
2025-05-27 11:48:48 -0700

    update index.js

--- test/SmokeTests/OOProcSmokeTests/durableJS/DurableFunctionsHttpStart/index.js
+++ test/SmokeTests/OOProcSmokeTests/durableJS/DurableFunctionsHttpStart/index.js
@@ -1,11 +1,11 @@
 ﻿const df = require("durable-functions");
 
 module.exports = async function (context, req) {
-  const client = df.getClient(context);
+    const client = df.getClient(context);
+    const body = await req.text();
+    const instanceId = await client.startNew("DurableFunctionsOrchestratorJS", { input: body });
 
-  const instanceId = await client.startNew("DurableFunctionsOrchestratorJS", undefined, req.body);
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
 
-  context.log(`Started orchestration with ID = '${instanceId}'.`);
-
-  return client.createCheckStatusResponse(context.bindingData.req, instanceId);
+    return client.createCheckStatusResponse(context.bindingData.req, instanceId);
 };
