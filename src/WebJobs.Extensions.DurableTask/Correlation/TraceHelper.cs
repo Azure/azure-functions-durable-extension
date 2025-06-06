27655ca03480c2f75953a293528d5bdce60ef7b3
Sophia Tevosyan <stevosyan@microsoft.com>
2025-06-05 18:04:04 -0700

    missed one file

--- src/WebJobs.Extensions.DurableTask/Correlation/TraceHelper.cs
+++ src/WebJobs.Extensions.DurableTask/Correlation/TraceHelper.cs
@@ -18,22 +18,14 @@ namespace Microsoft.Azure.WebJobs.Extensions.DurableTask.Correlation
 
         private static readonly ActivitySource ActivityTraceSource = new ActivitySource(Source);
 
-<<<<<<< HEAD
         internal static Activity? StartActivityForNewOrchestration(ExecutionStartedEvent startEvent, ActivityContext parentTraceContext, DateTimeOffset? startTime = default)
-=======
-        internal static Activity? StartActivityForNewOrchestration(ExecutionStartedEvent startEvent, ActivityContext parentTraceContext)
->>>>>>> dev
         {
             // Start the new activity to represent scheduling the orchestration
             Activity? newActivity = ActivityTraceSource.StartActivity(
                 Schema.SpanNames.CreateOrchestration(startEvent.Name, startEvent.Version),
                 kind: ActivityKind.Producer,
-<<<<<<< HEAD
                 parentContext: parentTraceContext,
                 startTime: startTime ?? default);
-=======
-                parentContext: parentTraceContext);
->>>>>>> dev
 
             if (newActivity == null)
             {
