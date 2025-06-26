0a35670ccf7f0f0c6efb38dccba94865c9ff71ff
Andy Staples <andystaples@microsoft.com>
2025-06-26 16:42:28 -0600

    Add timeout test

---
+++ test/e2e/Apps/BasicPowerShell/LongActivity/run.ps1
@@ -0,0 +1,4 @@
+param($name)
+
+Start-Sleep -Milliseconds 5000
+"The activity function completed successfully"
---
+++ test/e2e/Apps/BasicPowerShell/TimeoutOrchestrator/run.ps1
@@ -0,0 +1,19 @@
+param($Context)
+
+$timeoutSeconds = $Context.Input
+
+$task1 = Invoke-DurableActivity -FunctionName 'LongActivity' -Input '' -NoWait
+$task2 = Start-DurableTimer -Duration (New-TimeSpan -Seconds $timeoutSeconds) -NoWait
+$firstResult = Wait-DurableTask -Task @($task1, $task2) -Any
+
+if ($firstResult -eq $task1) {
+    Write-Host "LongActivity completed first, returning result"
+    Stop-DurableTimerTask -Task $task2
+    Get-DurableTaskResult -Task $task1
+} elseif ($firstResult -eq $task2) {
+    Write-Host "Timer completed first, returning 'Timeout'"
+    'The activity function timed out'
+} else {
+    Write-Host "Unexpected result type, returning 'Timeout'"
+    'The activity function timed out'
+}
