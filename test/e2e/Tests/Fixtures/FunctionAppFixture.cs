fc02e2f955c0adb23f5f76a34bef34b12f27a9ed
Andy Staples <andystaples@microsoft.com>
2025-07-10 09:34:16 -0600

    Add extra debug logging for test

--- test/e2e/Tests/Fixtures/FunctionAppFixture.cs
+++ test/e2e/Tests/Fixtures/FunctionAppFixture.cs
@@ -24,6 +24,7 @@ public class FunctionAppFixture : IAsyncLifetime
 
         string? e2eTestLanguageEnvVarValue = Environment.GetEnvironmentVariable("E2E_TEST_FUNCTIONS_LANGUAGE");
         this.logger.LogInformation("E2E_TEST_FUNCTIONS_LANGUAGE set to " + e2eTestLanguageEnvVarValue);
+        Console.WriteLine("E2E_TEST_FUNCTIONS_LANGUAGE set to " + e2eTestLanguageEnvVarValue);
         switch ((e2eTestLanguageEnvVarValue ?? "").ToLowerInvariant())
         {
             case "dotnet-isolated":
