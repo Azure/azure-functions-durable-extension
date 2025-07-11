2e8a417cb63a70bcf5a68a2d99384bd1a822030c
Andy Staples <andystaples@microsoft.com>
2025-07-11 13:47:25 -0600

    Skip invalid PS-MSSQL test

--- test/e2e/Tests/Tests/ErrorHandlingTests.cs
+++ test/e2e/Tests/Tests/ErrorHandlingTests.cs
@@ -60,6 +60,7 @@ public class ErrorHandlingTests
     }
 
     [Fact]
+    [Trait("PowerShell-MSSQL", "Skip")] // Bug: https://github.com/Azure/azure-functions-durable-powershell/issues/98
     public async Task OrchestratorWithCaughtActivityException_ShouldSucceed()
     {
         using HttpResponseMessage response = await HttpHelpers.InvokeHttpTrigger("StartOrchestration", "?orchestrationName=CatchActivityException");
