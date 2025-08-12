b43aff8ba1d3ab9a1b92fe5bbf241774d980d042
naiyuantian@microsoft.com <naiyuantian@microsoft.com>
2025-08-12 13:04:07 -0700

    udpate version and remove unnecessary using

--- src/WebJobs.Extensions.DurableTask/AzureStorageDurabilityProviderFactory.cs
+++ src/WebJobs.Extensions.DurableTask/AzureStorageDurabilityProviderFactory.cs
@@ -4,7 +4,6 @@
 using System;
 using DurableTask.AzureStorage;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask.Storage;
-using Microsoft.Azure.WebJobs.Host.Scale;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
