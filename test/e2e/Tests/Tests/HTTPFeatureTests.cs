efbfb69cdc734d215565bcdf9f6482c07bfdaff9
Sophia Tevosyan <stevosyan@microsoft.com>
2025-09-17 20:03:42 -0700

    trying to print logs to output to see why the CI is failing

--- test/e2e/Tests/Tests/HTTPFeatureTests.cs
+++ test/e2e/Tests/Tests/HTTPFeatureTests.cs
@@ -76,6 +76,11 @@ public class HttpFeatureTests
         
         if (isGitHubCI)
         {
+            foreach (var log in this.fixture.TestLogs.CoreToolsLogs)
+            {
+                this.output.WriteLine(log);
+            }
+
             // In GitHub CI, verify that the error message indicates failure due to absence of valid token credentials.
             // Check output to verify CallHttpAsync fails.
             Assert.Contains("Token source HTTP call failed", orchestrationDetails.Output);
