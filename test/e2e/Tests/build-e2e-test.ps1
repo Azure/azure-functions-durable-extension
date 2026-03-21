b6f29d046485af831deb1de5f0f9d3ea63d72658
Bernd Verst <beverst@microsoft.com>
2026-03-21 12:36:24 -0700

    WIP: Try more performant emulator for CI

--- test/e2e/Tests/build-e2e-test.ps1
+++ test/e2e/Tests/build-e2e-test.ps1
@@ -276,12 +276,12 @@ function StartMSSQLContainer($mssqlPwd) {
 }
 
 function StartDTSContainer() {
-  Write-Host "Pulling down the mcr.microsoft.com/dts/dts-emulator:latest image..."
-  docker pull mcr.microsoft.com/dts/dts-emulator:latest
+  Write-Host "Pulling down the berndverst/dtsemulator:test2 image..."
+  docker pull berndverst/dtsemulator:test2
 
   # Start the DTS Server docker container with the specified edition
   Write-Host "Starting DTS docker container on port 8080" -ForegroundColor DarkYellow
-  docker run -i -p 8080:8080 -p 8082:8082 -d mcr.microsoft.com/dts/dts-emulator:latest
+  docker run -i -p 8080:8080 -p 8082:8082 -d berndverst/dtsemulator:test2
 
   if ($LASTEXITCODE -ne 0) {
       exit $LASTEXITCODE
